00c4388d8ea478b5dceefd159c1b6d2b2f9d0f20
Luca <lcabrambilla@gmail.com>
2026-01-13 19:54:33 +0100

    Change BASE_DIR path, again again

--- finance_app/settings.py
+++ finance_app/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'finance_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # ← Questo deve esserci!
+        'DIRS': os.path.join(BASE_DIR, 'templates'),  # ← Questo deve esserci!
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
